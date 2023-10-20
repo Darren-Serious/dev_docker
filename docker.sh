0bd269f036504b370e8a677a02bd5544f8051a08
Darren <Darren_Serious.hotmail.com>
2023-10-20 09:30:00 +0000

    ref(shell): unite docker build and run

--- docker.sh
+++ docker.sh
@@ -4,26 +4,12 @@ tag=0.2
 
 build() {
   sudo docker build -f ./Dockerfile -t env_ede:$tag .
-};
-
-run() {
   sudo docker run -it -v /home:/home --name env_ede env_ede:$tag
-  echo "run"
-};
-
-remove() {
-  echo "remove"
 };
 
 if [ $1 == "build" ]
 then
   build
-elif [ $1 == "run" ]
-then
-  run
-elif [ $1 == "remove" ]
-then
-  remove
 else
   echo "没有符合的命令"
 fi
