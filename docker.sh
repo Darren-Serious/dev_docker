0f15bae3f1f97d201a736953c7fd095cdf5e3762
Darren <Darren_Serious.hotmail.com>
2023-11-11 00:18:44 +0800

    fix(permission): remove sudo permission

--- docker.sh
+++ docker.sh
@@ -3,16 +3,16 @@
 tag=0.2
 
 build() {
-  sudo docker build -f ./Dockerfile -t env_ede:$tag .
-  sudo docker run -it --privileged -v /home:/home -v /dev/bus/usb:/dev/bus/usb --name env_ede env_ede:$tag
+  docker build -f ./Dockerfile -t env_ede:$tag .
+  docker run -it --privileged -v /home:/home -v /dev/bus/usb:/dev/bus/usb --name env_ede env_ede:$tag
 };
 
 remove() {
   if [[ -n $(sudo docker ps -q -f "name=^env_ede$") ]];then
-    sudo docker stop env_ede
+    docker stop env_ede
   fi
-  sudo docker rm env_ede
-  sudo docker rmi env_ede:$tag
+  docker rm env_ede
+  docker rmi env_ede:$tag
 }
 
 if [ $1 == "build" ]
